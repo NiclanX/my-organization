bae999eab8942e70e0d4b2f889383d43c1dde8d3
NiclanX <nicardolannaman@gmail.com>
2023-07-22 16:05:44 -0500

    Initial Form

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
 
   return (
     <>
+    <div className="container">
       <SignIn/>
+    </div>
     </>
   )
 }
--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './App.css'
 
 export const SignIn = () => {
   return (
-    <form action="">
-        <h1>Hola</h1>
-        <input type="text" />
-        <input type="submit" />
+    <form action="GET">
+        <h1>Welcom back!</h1>
+        <input type="text" placeholder='First name' id='name'/><br />
+        <label htmlFor="name">Only enter your first name***</label><br />
+        <input type="submit" value='Sign In'/>
     </form>
   )
 }
