79dbb004d28cdb45c6d07e348701469f2700fd8e
NiclanX <nicardolannaman@gmail.com>
2023-07-22 15:40:21 -0500

    Committed

---
+++ src/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SignIn = () => {
+  return (
+    <form action="">
+        <h1>Hola</h1>
+        <input type="text" />
+        <input type="submit" />
+    </form>
+  )
+}
