e1356539857029c4f8ba4b674c72ffa093f1bac7
NiclanX <nicardolannaman@gmail.com>
2023-07-22 17:30:55 -0500

    Form 1.2

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 export const SignIn = () => {
-  function logIn(e) {
+  const [name, setName] = useState("")
+  
+  function handleSubmit(e) {
     e.preventDefault()
     
-    console.log(' is logged in')
   }
+
+  function nameInput(e){
+    setName(e.target.value)
+    
+  }
+
+  function logIn(){
+    if (name == "") {
+      alert('Please Type Your Name');
+
+    } else {
+      console.log(name + ' is eligible for log in')
+    }
+  }
+
   return (
-    <form action="GET">
-        <h1>Welcome back!</h1>
-        <input type="text" placeholder='First name' id='name' autoComplete='off'/><br />
+    <form onSubmit={handleSubmit}>
+        <h1>Welcome Back!</h1>
+        <input type="text" id='name' autoComplete='off' value={ name } onChange={(e) => nameInput(e)}/><br />
         <label htmlFor="name">Only enter your first name***</label><br />
         <button className='signIn-btn' onClick={logIn}>Sign In</button>
     </form>
