9df564e89c37221dcb82c6f2ef6d3d5ae56c51f6
NiclanX <nicardolannaman@gmail.com>
2023-07-22 17:04:34 -0500

    Form 1.1

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './App.css'
 
 export const SignIn = () => {
+  function logIn(e) {
+    e.preventDefault()
+    
+    console.log(' is logged in')
+  }
   return (
     <form action="GET">
-        <h1>Welcom back!</h1>
+        <h1>Welcome back!</h1>
         <input type="text" placeholder='First name' id='name' autoComplete='off'/><br />
         <label htmlFor="name">Only enter your first name***</label><br />
-        <button className='signIn-btn'>Sign In</button>
+        <button className='signIn-btn' onClick={logIn}>Sign In</button>
     </form>
   )
 }
